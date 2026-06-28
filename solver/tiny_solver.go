8ec42c84a2450100213122bb9db10d4d6a9ae165
kotvmeske-rii <kotvmeske@gmail.com>
2026-06-28 22:55:44 +0300

    Clean up comments in solver.go

--- solver/tiny_solver.go
+++ solver/tiny_solver.go
@@ -5,6 +5,9 @@ import (
 	"iglp/syntax"
 )
 
+// ModelWorld описывает отдельный мир в модели Крипке
+// Храним заведомо истиные и ложные формулы
+// свой номер(ID) и означивание
 type ModelWorld struct {
 	Number       int
 	Valuation    map[FormulaNumber]bool
@@ -12,12 +15,17 @@ type ModelWorld struct {
 	FalseFormula []FormulaNumber
 }
 
+// Шкала Крипке - пара из миров
+// и бинарного отношения достижимости между ними
 type KripkeFrame struct {
 	Worlds    map[int]*ModelWorld
 	Relations map[int][]int
 }
 
+// Рекурсивно вычисляем истинность формулы в заданном мире
+// true - формула истинна
 func CheckFormula(ctx context.Context, number FormulaNumber, worldN int, kripkeFrame *KripkeFrame) bool {
+	// Достаем библиотеку формул
 	bibliothek := GetContext(ctx)
 	key := bibliothek.Key(number)
 
@@ -25,16 +33,22 @@ func CheckFormula(ctx context.Context, number FormulaNumber, worldN int, kripkeF
 	case syntax.TokVar:
 		return kripkeFrame.Worlds[worldN].Valuation[number]
 	case syntax.TokFalse:
+		// Константа "ложь" - ложна в любом мире
 		return false
 	case syntax.TokTrue:
+		// Константа "истина" - истинна в любом мире
 		return true
 	case syntax.TokNot:
+		// Истинно, если подформула ложна
 		return !CheckFormula(ctx, key.Left, worldN, kripkeFrame)
 	case syntax.TokBox:
+		// []A истинна в мире w, если А истинна во всех мирах, достижимых из w
+		// Примечание: помним, что должна сохраняться транзитивность
 		achievWorld := kripkeFrame.Relations[worldN]
 
 		for _, childWorldN := range achievWorld {
 			if !CheckFormula(ctx, key.Left, childWorldN, kripkeFrame) {
+				//нашли мир где подформула ложна
 				return false
 			}
 		}
@@ -46,6 +60,8 @@ func CheckFormula(ctx context.Context, number FormulaNumber, worldN int, kripkeF
 		return CheckFormula(ctx, key.Left, worldN, kripkeFrame) ||
 			CheckFormula(ctx, key.Right, worldN, kripkeFrame)
 	case syntax.TokImpl:
+		//Примечание: для интуиционисткой логики определение будет иным
+		//(истинность проверяется во всех достижимых мирах)
 		return !CheckFormula(ctx, key.Left, worldN, kripkeFrame) ||
 			CheckFormula(ctx, key.Right, worldN, kripkeFrame)
 	}
